c38d089142da169bb0b6b5ed8829afe30a8e91d9
tanmay joshi <tanmayjoshi411@gmail.com>
2025-08-20 22:57:05 +0530

    sql

--- config/db.js
+++ config/db.js
@@ -1,4 +1,23 @@
-const mysql = require("mysql2/promise");
-const pool = mysql.createPool(process.env.MYSQL_URL);
-module.exports = pool;
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,  // added explicitly
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("✅ Connected to MySQL Database!");
+});
+
+export default connection;
 
