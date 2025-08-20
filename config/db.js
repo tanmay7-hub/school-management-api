6d27de38f8142344572155b3565e75fcc2c4a228
tanmay joshi <tanmayjoshi411@gmail.com>
2025-08-20 22:44:27 +0530

    new changes applies

--- config/db.js
+++ config/db.js
@@ -1,12 +1,4 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-
-dotenv.config();
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-}).promise();
-
+const mysql = require("mysql2/promise");
+const pool = mysql.createPool(process.env.MYSQL_URL);
 module.exports = pool;
+
